0441ac3b2608fe60efc1a740196c34fd3d480674
robertovers <oversr14@gmail.com>
2022-08-01 21:36:36 +1000

    Add run method

--- application.cpp
+++ application.cpp
@@ -3,4 +3,23 @@
 
 Application::Application() { }
 
-void Application::run() { }
+void Application::run() { 
+
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Insect Simulations");
+
+    while (window.isOpen()) {
+
+        sf::Event event;
+
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+        // update and draw objects here
+
+        window.display();
+    }
+}
