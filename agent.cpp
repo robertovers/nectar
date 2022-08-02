90c4c297c68631044bedc08b4050901221cc03ad
robertovers <oversr14@gmail.com>
2022-08-02 13:37:47 +1000

    Add basic Agent class implementation

--- agent.cpp
+++ agent.cpp
@@ -1,1 +1,11 @@
 #include "agent.hpp"
+
+Agent::Agent(float x, float y) {
+    pos_x = x;
+    pos_y = y;
+}
+
+void Agent::update() {
+    pos_x += vel_x;
+    pos_y += vel_y;
+}
--- agent.hpp
+++ agent.hpp
@@ -1,4 +1,15 @@
 #ifndef agent_hpp
 #define agent_hpp
 
+class Agent {
+    public:
+        Agent(float x, float y);
+        void update();
+    private:
+        float pos_x;
+        float pos_y;
+        float vel_x = 0;
+        float vel_y = 0;
+};
+
 #endif
