546df6db7a83a6b064b6323386fda3380bd5bb8a
robertovers <oversr14@gmail.com>
2022-08-04 23:29:58 +1000

    Fix loops over reference wrapper

--- agentController.cpp
+++ agentController.cpp
@@ -9,13 +9,13 @@ void AgentController::addAgent(Agent& a) {
 }
 
 void AgentController::updateAgents() {
-    for (auto& a : agents) {
+    for (auto a : agents) {
         a.get().update();
     }
 }
 
 void AgentController::renderAgents(sf::RenderWindow& window) {
-    for (auto& a : agents) {
+    for (auto a : agents) {
         a.get().render(window);
     }
 }
