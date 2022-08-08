77527ae2e59abeb7a14a6463b6457507262cffd4
Lucille Joyce Grasso <lgra0008@student.monash.edu>
2022-08-08 16:14:41 +1000

    mapgenerater class definitions

--- mapGenerator.hpp
+++ mapGenerator.hpp
@@ -1,4 +1,27 @@
 #ifndef mapgenerator_hpp
 #define mapgenerator_hpp
 
+#include "environment.hpp"
+
+// Generates a populated map for the simulation to run on
+class MapGenerator {
+public:
+	virtual Environment generateEnvironment() = 0;
+};
+
+// Generates an environment with agents and objects randomly scattered around
+class BasicMapGenerator : MapGenerator {
+private:
+	int envSizeX;
+	int envSizeY;
+	int beeCount;
+	int waspCount;
+	int flowerCount;
+	int cropCount;
+	int caterpillarCount;
+
+public:
+	BasicMapGenerator(int envSizeX, int envSizeY, int beeCount, int waspCount, int flowerCount, int cropCount, int caterpillarCount);
+	Environment generateEnvironment();
+};
 #endif
