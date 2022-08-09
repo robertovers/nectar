028b0969361851eb654f22687e8fb7da66cecd4a
Robert Overs <69703259+robertovers@users.noreply.github.com>
2022-08-09 12:37:38 +1000

    added base environment class (#8)
    Co-authored-by: ethancheng <eche0022@student.monash.edu>

--- environment.hpp
+++ environment.hpp
@@ -1,7 +1,7 @@
 #ifndef environment_hpp
 #define environment_hpp
 
-class Environment {
+class Environment : public sf::Drawable, public sf::Transformable {
     public:
         int getSize() { return 0; };
 };
