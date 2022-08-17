9301449f517b6d0fff08e4a0bd0a930f4ca499a3
robertovers <oversr14@gmail.com>
2022-08-18 00:16:16 +1000

    Add getLocation implementation for 2D array

--- agent.cpp
+++ agent.cpp
@@ -1,24 +1,13 @@
-#include <SFML/Graphics.hpp>
+#include <math.h>
 #include "agent.hpp"
 
 Agent::~Agent() { }
 
-std::shared_ptr<Location> Agent::getLocation(Environment env) {
-    for (auto loc : env.getLocations()) {
-        int tile_x = loc->getX()*loc->getWidth();
-        int tile_y = loc->getY()*loc->getHeight();
-        if ( 
-            pos_x+display_width > tile_x and 
-            pos_x+display_width <= tile_x + loc->getWidth() and
-            pos_y+display_height > tile_y and 
-            pos_y+display_height <= tile_y + loc->getHeight() )
-            {
-                return loc;
-            }
-    }
-    return std::make_shared<Location>(); 
+std::shared_ptr<Location>& Agent::getLocation(Environment env) {
+    vector2D_shared_ptr<Location>& locations = env.getLocations();
+    return locations[floor(pos_y)][floor(pos_x)]; 
 }
 
-std::shared_ptr<Location> Agent::getTarget() {
+std::shared_ptr<Location>& Agent::getTarget() {
     return target;
 }
