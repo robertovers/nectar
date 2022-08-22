b7d748abde49067dc99118621f26aeaec3a4a0b6
Lucille Joyce Grasso <lgra0008@student.monash.edu>
2022-08-22 22:48:00 +1000

    add mapgenerator comments

--- src/mapGenerator.hpp
+++ src/mapGenerator.hpp
@@ -19,6 +19,15 @@ private:
 	int cropChance;
 
 public:
+	/**
+	* Settings for generated map
+	*
+	* @param envSizeX	 Length of the map
+	* @param envSizeY	 Height of the map
+	* @param beeCount	 Number of bees in the map
+	* @param cropChance  Chance that a location will have a crop on it, out of 100.
+	*					 For example, cropChance = 50 means 50/100 = 1/2 chance
+	*/
 	BasicMapGenerator(int envSizeX, int envSizeY, int beeCount, int cropChance);
 	Environment generateEnvironment(AgentController& agentController);
 };
