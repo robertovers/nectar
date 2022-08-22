bc27c368e10b6601dca338f4aa34d85da8386347
Lucille Joyce Grasso <lgra0008@student.monash.edu>
2022-08-22 21:51:14 +1000

    Simplify crop count to crop chance

--- src/mapGenerator.cpp
+++ src/mapGenerator.cpp
@@ -5,7 +5,7 @@
 #include "location.hpp"
 #include "honeybee.hpp"
 
-BasicMapGenerator::BasicMapGenerator(int envSizeX, int envSizeY, int beeCount = 0, int cropCount = 0) {
+BasicMapGenerator::BasicMapGenerator(int envSizeX, int envSizeY, int beeCount = 0, int cropChance = 0) {
     if (envSizeX <= 0) {
         throw std::invalid_argument("X value must be positive integer");
     }
@@ -16,7 +16,7 @@ BasicMapGenerator::BasicMapGenerator(int envSizeX, int envSizeY, int beeCount = 
     this->envSizeX = envSizeX;
     this->envSizeY = envSizeY;
     this->beeCount = beeCount;
-    this->cropCount = cropCount;
+    this->cropChance = cropChance;
 }
 
 
@@ -33,18 +33,15 @@ Environment BasicMapGenerator::generateEnvironment(AgentController& agentControl
         placedBees++;
     }
 
-    int placedCrops = 0;
     // place plants on locations
+    float placeChance = cropChance / 100.0;
     for (int tilesLeft = generatedEnvironment.getSize(); tilesLeft > 0; tilesLeft -= 1) {
         // TODO: properly iterate through tiles in environment
         Location currentLocation = Location();
 
-        // random chance to place a crop
-        float placeChance = (static_cast<float>(cropCount) - placedCrops) / tilesLeft;
         float rolledChance = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);  // 0.0 - 1.0
         if (placeChance >= rolledChance) {
             // TODO: place crop
-            placedCrops += 1;
         }
     }
     return generatedEnvironment;
