5fee0bf02093b43ecaa263c76104fa731e5c548c
JirehChen <jche0212@student.monash.edu>
2022-08-23 22:54:11 +1000

    Update soybean.cpp
    Implemented draw function for Soybean, as a green cross shape.
    
    [CrossShape class for general use between plants in development, with many issues.. However shape creation in Soybean::draw should ideally be substituted later]

--- src/soybean.cpp
+++ src/soybean.cpp
@@ -2,6 +2,7 @@
 #define SOYBEAN_CPP
 
 #include "soybean.hpp"
+#include "crossShape.hpp"
 #include <iostream>
 #define GENDER 3
 
@@ -19,7 +20,25 @@ void Soybean::update(Environment env){
 }
 
 void Soybean::draw(sf::RenderWindow& window){
+    float diameter = 35;
+    sf::ConvexShape c;
+    c.setPointCount(12);
+    c.setPoint(0, sf::Vector2f(pos_x - d/6,pos_y - d/2));
+    c.setPoint(1, sf::Vector2f(pos_x + d/6,pos_y - d/2));
+    c.setPoint(2, sf::Vector2f(pos_x + d/6,pos_y - d/6));
+    c.setPoint(3, sf::Vector2f(pos_x + d/2,pos_y - d/6));
+    c.setPoint(4, sf::Vector2f(pos_x + d/2,pos_y + d/6));
+    c.setPoint(5, sf::Vector2f(pos_x + d/6,pos_y + d/6));
+    c.setPoint(6, sf::Vector2f(pos_x + d/6,pos_y + d/2));
+    c.setPoint(7, sf::Vector2f(pos_x - d/6,pos_y + d/2));
+    c.setPoint(8, sf::Vector2f(pos_x - d/6,pos_y + d/6));
+    c.setPoint(9, sf::Vector2f(pos_x - d/2,pos_y + d/6));
+    c.setPoint(10, sf::Vector2f(pos_x - d/2,pos_y - d/6));
+    c.setPoint(11, sf::Vector2f(pos_x - d/6,pos_y - d/6));
+    c.setFillColor(sf::Color(100, 250, 50));
+    window.draw(c);
 }
+
 
 
 
