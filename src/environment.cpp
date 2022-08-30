b386095b8aaa2c65129cc83b3c68c816875ea670
robertovers <oversr14@gmail.com>
2022-08-30 22:53:17 +1000

    Add random Hive in environment constructor

--- src/environment.cpp
+++ src/environment.cpp
@@ -9,10 +9,20 @@ Environment::Environment(int w, int h) : width(w), height(h) {
             locations[y][x] = std::make_shared<Location>(x, y);
         }
     }
+
+    int hive_x = rand() % w;
+    int hive_y = rand() % h;
+    hive = std::make_shared<Hive>(hive_x, hive_y);
+
+    locations[hive_y][hive_x] = hive;
 }
 
 vector2D_shared_ptr<Location>& Environment::getLocations() { 
     return locations; 
+}
+
+shared_ptr<Location>& Environment::getHive() {
+    return hive;
 }
 
 int Environment::getSize() {
