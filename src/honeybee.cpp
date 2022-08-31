ee0e4c8533686266fc3cce20c4dad7f3b8b1e8c3
robertovers <oversr14@gmail.com>
2022-08-31 17:59:17 +1000

    Honeybee searches and moves towards nearby flowers

--- src/honeybee.cpp
+++ src/honeybee.cpp
@@ -16,10 +16,15 @@ HoneyBee::HoneyBee(float x, float y) {
 
 void HoneyBee::update(Environment env) {
 
-    shared_ptr<Location> old_loc = getLocation(env);
+    shared_ptr<Location> cur_loc = getLocation(env);
 
-    if (target == nullptr) { 
-        moveRandomWalk();
+    if (target == nullptr and cur_loc != nullptr) { 
+        auto found = scan(env);
+        if (found == nullptr) {
+            moveRandomWalk();
+        } else {
+            target = found;
+        }
     } else if (getLocation(env) == target) {
         target = nullptr;
         moveRandomWalk();
@@ -31,11 +36,30 @@ void HoneyBee::update(Environment env) {
 
     shared_ptr<Location> new_loc = getLocation(env);
 
-    if (new_loc != old_loc) {
-        old_loc->removeAgent(*this);
+    if (new_loc != nullptr && cur_loc != nullptr && new_loc != cur_loc) {
+        cur_loc->removeAgent(*this);
         new_loc->addAgent(*this);
     }
 }
+
+shared_ptr<Location> HoneyBee::scan(Environment env) {
+    auto current_loc = getLocation(env);
+    auto locations = env.getLocations();
+
+    for (int ix=-2; ix<=2; ix++) {
+        for (int iy=-2; iy<=2; iy++) {
+            int tile_x = current_loc->getX() + ix;
+            int tile_y = current_loc->getY() + iy;
+            if ( tile_x >= 0 && tile_x < env.getWidth() &&
+                 tile_y >= 0 && tile_y < env.getHeight() ) 
+            {
+                auto loc = locations[tile_y][tile_x]; 
+                if (loc->hasNectar()) return loc;
+            }
+        }
+    }
+    return nullptr;
+} 
 
 void HoneyBee::draw(sf::RenderWindow &window) {
     sf::CircleShape c;
