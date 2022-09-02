96e5ae2244badb03eeff772f7c266685e277195d
Lucille Joyce Grasso <lgra0008@student.monash.edu>
2022-09-02 15:40:13 +1000

    basicmapgenerator environment size tests

--- test/mapgenerator_test.cc
+++ test/mapgenerator_test.cc
@@ -1,9 +1,35 @@
 #include <gtest/gtest.h>
+#include "../src/basicMapGenerator.hpp"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+
+TEST(BasicMapGeneratorTest, MapSize) {
+    // Invalid map size options
+    EXPECT_THROW(BasicMapGenerator(-1, 1, 0, 0), std::invalid_argument);
+    EXPECT_THROW(BasicMapGenerator(0, 1, 0, 0), std::invalid_argument);
+    EXPECT_THROW(BasicMapGenerator(1, -1, 0, 0), std::invalid_argument);
+    EXPECT_THROW(BasicMapGenerator(-1, 0, 0, 0), std::invalid_argument);
+    
+    // Check that generated environment is right size
+    int x = 3;
+    int y = 4;
+    int bees = 3;
+    int cropChance = 50;
+    Environment env = BasicMapGenerator(x, y, bees, cropChance).generateEnvironment()
+    EXPECT_EQ(env.getSize(), x * y);
+    EXPECT_EQ(env.getWidth(), x);
+    EXPECT_EQ(env.getHeight(), y);
+}
+
+TEST(BasicMapGeneratorTest, BeeCount) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(BasicMapGeneratorTest, SoybeanCount) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
 }
