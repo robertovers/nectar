d0e8ab0a3d68be55acb213cfe4319dc5c2e0e626
Lucille Joyce Grasso <lgra0008@student.monash.edu>
2022-09-05 22:07:19 +1000

    location_test agent scaffolding

--- test/location_test.cpp
+++ test/location_test.cpp
@@ -1,6 +1,26 @@
 #include <gtest/gtest.h>
 #include "../src/location.hpp"
 
-TEST(LocationTest, Something) {
-    
+// agents are added
+TEST(LocationTest, AddAgent) {
+    // add new agent
+
+    // re-add existing agent
+}
+
+// number of agents is reported
+TEST(LocationTest, AgentCount) {
+
+}
+
+// agents are removed
+TEST(LocationTest, RemoveAgent) {
+    // remove existing agent
+
+    // try to remove non-existing agent
+}
+
+// all agents are removed
+TEST(LocationTest, ClearAgents) {
+
 }
