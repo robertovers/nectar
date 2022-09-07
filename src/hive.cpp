5d046400745eb08449cc3b1998097dcdac9e7c55
Lucille Joyce Grasso <lgra0008@student.monash.edu>
2022-09-07 23:39:03 +1000

    Hive refactor

--- src/hive.cpp
+++ src/hive.cpp
@@ -2,8 +2,15 @@
 
 void Hive::draw(sf::RenderWindow& window) {
     sf::RectangleShape r;
-    r.setSize(sf::Vector2f(20, 20));
-    r.setPosition(x * 20, y * 20);
+    auto windowSize = window.getSize();
+    float width = 0;
+    // set square to fit smallest window dimension
+    if (windowSize.x < windowSize.y) {
+        width = drawWidth * windowSize.x;
+    }
+    else {
+        width = drawWidth * windowSize.y;
+    }
     r.setFillColor(sf::Color::Red);
     window.draw(r);
 }
