af9862cc4f93bb86786b6bc8e5a54a082ed39918
Robert Overs <oversr14@gmail.com>
2022-09-09 22:12:11 +1000

    Fix missing draw method

--- src/plant.cpp
+++ src/plant.cpp
@@ -5,8 +5,20 @@ Plant::~Plant() { }
 Plant::Plant(int x, int y) : Location(x, y) {
 }
 
+void Plant::draw(sf::RenderWindow& window) {
+    sf::RectangleShape r;
+    r.setSize(sf::Vector2f(20, 20));
+    r.setPosition(x * 20, y * 20);
+    r.setFillColor(sf::Color(0, 50, 0));
+    window.draw(r);
+}
+
 bool Plant::isPlant() {
     return true;
+}
+
+bool Plant::isPollinated() {
+    return pollinated;
 }
 
 void Plant::pollinate() {
