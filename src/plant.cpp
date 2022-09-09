b692da23cbf550ad9466190663434627fc9b3888
Robert Overs <oversr14@gmail.com>
2022-09-09 21:49:53 +1000

    Move draw to Soybean

--- src/plant.cpp
+++ src/plant.cpp
@@ -5,37 +5,12 @@ Plant::~Plant() { }
 Plant::Plant(int x, int y) : Location(x, y) {
 }
 
-void Plant::draw(sf::RenderWindow& window) {
-    /*float d = 35;
-     sf::ConvexShape c;
-     c.setPointCount(12);
-     c.setPoint(0, sf::Vector2f(x - d/6, y - d/2));
-     c.setPoint(1, sf::Vector2f(x + d/6, y - d/2));
-     c.setPoint(2, sf::Vector2f(x + d/6, y - d/6));
-     c.setPoint(3, sf::Vector2f(x + d/2, y - d/6));
-     c.setPoint(4, sf::Vector2f(x + d/2, y + d/6));
-     c.setPoint(5, sf::Vector2f(x + d/6, y + d/6));
-     c.setPoint(6, sf::Vector2f(x + d/6, y + d/2));
-     c.setPoint(7, sf::Vector2f(x - d/6, y + d/2));
-     c.setPoint(8, sf::Vector2f(x - d/6, y + d/6));
-     c.setPoint(9, sf::Vector2f(x - d/2, y + d/6));
-     c.setPoint(10, sf::Vector2f(x - d/2, y - d/6));
-     c.setPoint(11, sf::Vector2f(x - d/6, y - d/6));
-     c.setFillColor(sf::Color(100, 250, 50));
-     window.draw(c);*/
-    sf::RectangleShape r;
-    r.setSize(sf::Vector2f(20, 20));
-    r.setPosition(x * 20, y * 20);
-    r.setFillColor(sf::Color(0, 50, 0));
-    window.draw(r);
-}
-
 bool Plant::isPlant() {
     return true;
 }
 
-void Plant::pollinate(float amount) {
-    pollen += amount;
+void Plant::pollinate() {
+    pollinated = true;
 }
 
 bool Plant::hasNectar() {
