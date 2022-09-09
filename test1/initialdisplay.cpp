115a31d583f707e446897b5e8834849da8c2d750
JirehChen <jche0212@student.monash.edu>
2022-09-09 20:56:49 +1000

    initialdisplay.cpp & openSans font
    Created class initialdisplay.cpp which renders a basic display with text
    
    Added a folder for text fonts used in simulation display.

---
+++ test1/initialdisplay.cpp
@@ -0,0 +1,60 @@
+#include <SFML/Graphics.hpp>
+#include <stdio.h>
+#include <iostream>
+#define PARAMETERS 1
+#define LINEHEIGHT 200
+#define WINDOWWIDTH 500
+
+int main(){
+    // Basic Simulation Params
+    int tiles, bees;                        // params in use
+    //enum mapGenerator, hiveLocation;
+    // Advanced Simulation params
+    //int sim_duration;                       // params not currently in use
+    sf::String p1_input;
+    sf::Text instructions, p1, confirm;
+
+    //Window for setting simulation parameters - extra space for line for comfirm button (1) and instructions (3)
+    sf::RenderWindow window(sf::VideoMode((PARAMETERS+5)*LINEHEIGHT, WINDOWWIDTH), "Simulation Parameters");
+    
+    // Set Font
+    sf::Font font;     
+    if (!font.loadFromFile("../fonts/open-sans/OpenSans-Regular.ttf"))
+    {
+        std::cout << "Error loading font" << "../fonts/open-sans/OpenSans-Regular.ttf" << std::endl;
+    }
+
+    //Window use instructions
+    // Start typing to enter parameter values
+    // Implement later// Press enter or tab to go to next field  //Implement later(cursor should follow)
+    // When confirm is highlighted press enter to start simulation
+    instructions.setFont(font);
+    instructions.setString("Instructions Placeholder\n.\n.\n");
+    instructions.setPosition(WINDOWWIDTH/10 , LINEHEIGHT/2);
+
+    p1.setFont(font);
+    p1.setString("Tiles: ");
+    p1.setPosition(WINDOWWIDTH/10, LINEHEIGHT*7/2);
+    
+    //text.setCharacterSize(10);
+    //text.setFillColor(sf::Color::Green);
+
+    while(window.isOpen()){
+
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            switch (event.type){
+                case sf::Event::Closed:
+                    window.close();
+                case sf::Event::TextEntered:
+                    std::cout << "ASCII character typed: " << static_cast<char>(event.text.unicode) << std::endl;
+            }
+        }
+        window.clear( sf::Color(127,127,127));
+        window.draw(instructions);
+        window.draw(p1);
+
+        window.display();
+    }
+}
+
