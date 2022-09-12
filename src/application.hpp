642a96ee04a0bd6070418afe61ec716af37d9f52
Robert Overs <69703259+robertovers@users.noreply.github.com>
2022-09-12 13:14:20 +1000

    Application, Agent, Honeybee & AgentController documentation (#29)
    * Agent & Environment documentation (#28)
    
    * Remove Soybean references
    
    * Move plant-related methods to Plant
    
    * Delete soybean class
    
    * Remove iostream includes
    
    * Add depositNectar method
    
    * Update harvestNectar method
    
    * Change default pollen value
    
    * Add Honeybee interaction with Location types
    
    * Change hive getter/setter to Hive pointer
    
    * Add nectar getter
    
    * Restore Soybean
    
    * Move draw to Soybean
    
    * Remove casting to Hive
    
    * Change pollen attribute to bool
    
    * Pass arguments by reference
    
    * Add pollination count
    
    * Pass arguments by reference
    
    * Fix missing draw method
    
    * Change colour
    
    * Add isPollinated method
    
    * Add pollination count to metrics
    
    * Resolving conflicts
    
    * Resolving conflicts
    
    * Add override specifier to honeybee draw method
    
    * Add Agent documentation
    
    * Add missing param documentation
    
    * Add Application class documentation
    
    * Removed unused functions
    
    * Set default nectar value
    
    * Add Honeybee class documentation
    
    * Add AgentController documentation
    
    * Initialise nectar attribute

--- src/application.hpp
+++ src/application.hpp
@@ -1,12 +1,47 @@
+/**
+ * FIT3161/3162 Computer Science Project
+ * Insect Simulation for Improved Pollination and Pest Control
+ * Group CS6
+ * 
+ * @file application.hpp
+ * @brief Application class that initiates and controls the simulation.
+ * @date 2022-09-10
+ */
+
 #ifndef application_hpp
 #define application_hpp
 
+/**
+ * @brief Application class that initiates and controls the simulation.
+ * 
+ * The Application class is the top of the simulation heirarchy, and contains
+ * the simulation loop that calls all update and render methods.
+ */
 class Application {
+
     public:
+
+        /**
+         * @brief Construct a new Application object.
+         */
         Application();
+
+        /**
+         * @brief Runs the simulation by initialising the simulation loop.
+         */
         void run();
+
     private:
-        // Returns transformation that will be applied to all objects in the simulation
+
+        /**
+         * @brief Returns transformation that will be applied to all objects in the simulation.
+         * 
+         * @param rows the number of rows of location tiles.
+         * @param columns the number of columns of location tiles.
+         * @param windowWidth the width of the display window in pixels.
+         * @param windowHeight the height of the display window in pixels.
+         * @return sf::Transform 
+         */
         sf::Transform spriteTransformation(int rows, int columns, int windowWidth, int windowHeight);
 };
 
--- src/honeybee.hpp
+++ src/honeybee.hpp
@@ -1,3 +1,13 @@
+/**
+ * FIT3161/3162 Computer Science Project
+ * Insect Simulation for Improved Pollination and Pest Control
+ * Group CS6
+ * 
+ * @file honeybee.hpp
+ * @brief Honeybee class for instances of the honeybee agent in the simulation.
+ * @date 2022-09-11
+ */
+
 #ifndef honeybee_hpp
 #define honeybee_hpp
 
@@ -5,14 +15,56 @@
 #include "agent.hpp"
 #include "location.hpp"
 
+/**
+ * @brief Honeybee class for instances of the honeybee agent in the simulation.
+ * 
+ * The Honeybee extends the Agent class. Its follows a simple behaviour - 
+ * searching for nearby plants and collecting nectar from them if found,
+ * then returning to the hive to deposit the nectar.
+ */
 class HoneyBee : public Agent {
+
     public:
+
+        /**
+         * @brief Construct a new Honeybee object.
+         */
         HoneyBee();
+
+        /**
+         * @brief Construct a new Honey Bee object at the passed coordinates.
+         * 
+         * @param x a float x-coordinate, from 0 to the tile-width.
+         * @param y a float y-coordinate, from 0 to the tile-height.
+         */
         HoneyBee(float x, float y);
+
+        /**
+         * @brief Updates the position and state of the Honeybee agent.
+         * 
+         * @param env a reference to the simulation environment.
+         */
         void update(Environment& env) override;
+
+        /**
+         * @brief Renders the Honeybee object to the target.
+         * 
+         * @param target a reference to a render target.
+         * @param states states to be applied when rendering to the target.
+         */
         void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-        shared_ptr<Location> scan(Environment env); 
+
+        /**
+         * @brief Searches a radius of the agent's location for a plant.
+         * 
+         * @param env the simulation environment.
+         * @return a shared pointer to a plant, or a null pointer if none found. 
+         */
+        shared_ptr<Location> scanForPlants(Environment env); 
+
     protected:
+
+        /// @brief The amount of nectar the agent is carrying.
         float nectar = 0;
 };
 
