718dd279cadd9a2defae19dc83b57ec78bd93d61
Robert Overs <oversr14@gmail.com>
2022-09-12 16:18:57 +1000

    Add test files

---
+++ test/honeybee_test.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include "../src/honeybee.hpp"
+#include "../src/location.hpp"
+#include "../src/basicMapGenerator.hpp"
+
+Test(HoneybeeTest, ScanLocations) {
+
+}
+
+Test(HoneybeeTest, MemoryIsAdded) {
+
+}
+
+Test(HoneybeeTest, MemoryIsLimited) {
+
+}
+
+Test(HoneybeeTest, MemoryIsFIFO) {
+
+}
