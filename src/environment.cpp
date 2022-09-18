5c034d4b264120f0ffee3d2aef7086d3eb810b2c
Robert Overs <oversr14@gmail.com>
2022-09-18 23:36:03 +1000

    Formatting

--- src/environment.cpp
+++ src/environment.cpp
@@ -18,6 +18,54 @@ void Environment::draw(sf::RenderTarget & target, sf::RenderStates states) {
     }
 }
 
+void Environment::initLookupTable() {
+    for (auto row : locations) {
+        for (auto loc : row) {
+            auto nearbyPlant = findNearbyPlant(loc);
+            if (nearbyPlant) {
+                plant_table.insert(std::make_pair(loc->getID(), *nearbyPlant));
+            }
+        }
+    }
+}
+
+opt_shared_ptr<Location> Environment::getNearbyPlant(shared_ptr<Location> loc) {
+    if (loc == nullptr) {
+        return { };
+    }
+    auto result = plant_table.find(loc->getID());
+    if (result != plant_table.end()) {
+        return result->second;
+    } else {
+        return { };
+    }
+}
+
+opt_shared_ptr<Location> Environment::findNearbyPlant(shared_ptr<Location> loc) {
+    vector_shared_ptr<Location> nearby;
+    for (int ix=-2; ix<=2; ix++) {
+        for (int iy=-2; iy<=2; iy++) {
+
+            int tile_x = loc->getX() + ix;
+            int tile_y = loc->getY() + iy;
+
+            if ( tile_x >= 0 && tile_x < this->getWidth() &&
+                 tile_y >= 0 && tile_y < this->getHeight() ) 
+            {
+                auto target = locations[tile_y][tile_x]; 
+
+                if (target->isPlant()) {
+                    nearby.push_back(target);
+                }
+            }
+        }
+    }
+    if (nearby.size() > 0) {
+        return nearby[rand() % nearby.size()];
+    }
+    return { };
+}
+
 vector2D_shared_ptr<Location>& Environment::getLocations() { 
     return locations; 
 }
@@ -78,52 +126,4 @@ void Environment::incPollinatedCount() {
 
 int Environment::getPollinatedCount() {
     return pollinated_count;
-}
-
-void Environment::initLookupTable() {
-    for (auto row : locations) {
-        for (auto loc : row) {
-            auto nearbyPlant = findNearbyPlant(loc);
-            if (nearbyPlant) {
-                plant_table.insert(std::make_pair(loc->getID(), *nearbyPlant));
-            }
-        }
-    }
-}
-
-opt_shared_ptr<Location> Environment::getNearbyPlant(shared_ptr<Location> loc) {
-    if (loc == nullptr) {
-        return { };
-    }
-    auto result = plant_table.find(loc->getID());
-    if (result != plant_table.end()) {
-        return result->second;
-    } else {
-        return { };
-    }
-}
-
-opt_shared_ptr<Location> Environment::findNearbyPlant(shared_ptr<Location> loc) {
-    vector_shared_ptr<Location> nearby;
-    for (int ix=-2; ix<=2; ix++) {
-        for (int iy=-2; iy<=2; iy++) {
-
-            int tile_x = loc->getX() + ix;
-            int tile_y = loc->getY() + iy;
-
-            if ( tile_x >= 0 && tile_x < this->getWidth() &&
-                 tile_y >= 0 && tile_y < this->getHeight() ) 
-            {
-                auto target = locations[tile_y][tile_x]; 
-
-                if (target->isPlant()) {
-                    nearby.push_back(target);
-                }
-            }
-        }
-    }
-    if (nearby.size() > 0) {
-        return nearby[rand() % nearby.size()];
-    }
-    return { };
 }
