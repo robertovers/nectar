129377013bb50963adc5aa953f85d539315885ad
Lucille Joyce Grasso <lgra0008@student.monash.edu>
2022-09-21 19:00:08 +1000

    Dummy radio buttons

--- src/display/legendsWindow.cpp
+++ src/display/legendsWindow.cpp
@@ -1,15 +1,17 @@
 #include "legendsWindow.hpp"
 
 void LegendsWindow::draw(int windowX, int windowY) {
+    // TODO: not define in function
     auto soybeanColor = ImVec4(0 / 255.0f, 50 / 255.0f, 35 / 255.0f, 255 / 255.0f); // 0, 50, 35
     auto nectarColor = ImVec4(187 / 255.0f, 205 / 255.0f, 17 / 255.0f, 255 / 255.0f); //sf::Color(187, 205, 17);
     auto pollenColor = ImVec4(247 / 255.0f, 215 / 255.0f, 90 / 255.0f, 255 / 255.0f); // (247, 215, 90)
+    int pollenOption = 1;
+    int nectarOption = 1;
     auto hiveColor = ImVec4(255 / 255.0f, 0 / 255.0f, 0 / 255.0f, 255 / 255.0f);
     //auto hiveColor = ImVec4();
     ImGui::Begin("Display Options");
     // 1. Plant options
-    ImGui::ColorEdit3("Soybean", (float*)&soybeanColor, ImGuiColorEditFlags_NoInputs);
-    ImGui::SameLine(); 
+    ImGui::ColorEdit3("Soybean", (float*)&soybeanColor, ImGuiColorEditFlags_NoInputs); ImGui::SameLine();
     if (ImGui::Button("Reset##1")) {
 
     }
@@ -18,28 +20,27 @@ void LegendsWindow::draw(int windowX, int windowY) {
     // TODO: header
     // TODO: reset color
     // todo: toggle overlay display (radio buttons
-    ImGui::ColorEdit3("Soybean with max nectar", (float*)&nectarColor, ImGuiColorEditFlags_NoInputs);
-    ImGui::SameLine();
+    ImGui::ColorEdit3("Soybean with max nectar", (float*)&nectarColor, ImGuiColorEditFlags_NoInputs); ImGui::SameLine();
     if (ImGui::Button("Reset##2")) {
 
     };
+    ImGui::RadioButton("None##2", &nectarOption, 0); ImGui::SameLine();
+    ImGui::RadioButton("Overlay##2", &nectarOption, 1);
     
     // 3. pollen options
     // TODO: header
     // TODO: reset color
     // todo: toggle overlay display
-    ImGui::ColorEdit3("Pollinated soybean", (float*)&pollenColor, ImGuiColorEditFlags_NoInputs);
-    ImGui::SameLine();
+    ImGui::ColorEdit3("Pollinated soybean", (float*)&pollenColor, ImGuiColorEditFlags_NoInputs); ImGui::SameLine();
     if (ImGui::Button("Reset##3")) {
 
     };
-    /*ImGui::RadioButton("None##3", &e, 0); ImGui::SameLine();
-    ImGui::RadioButton("Small square", &e, 1); ImGui::SameLine();
-    ImGui::RadioButton("Full square", &e, 2);*/
+    ImGui::RadioButton("None##3", &pollenOption, 0); ImGui::SameLine();
+    ImGui::RadioButton("Small square", &pollenOption, 1); ImGui::SameLine();
+    ImGui::RadioButton("Overlay##3", &pollenOption, 2);
 
     // 4. Hive options
-    ImGui::ColorEdit3("Hive", (float*)&hiveColor, ImGuiColorEditFlags_NoInputs);
-    ImGui::SameLine();
+    ImGui::ColorEdit3("Hive", (float*)&hiveColor, ImGuiColorEditFlags_NoInputs); ImGui::SameLine();
     if (ImGui::Button("Reset##4")) {
 
     };
