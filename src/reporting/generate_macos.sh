e6c3472aa822f99104aee48c1caf80e9712ce388
Robert Overs <oversr14@gmail.com>
2022-09-30 15:04:18 +1000

    Add matplotlib to scripts

--- src/reporting/generate_macos.sh
+++ src/reporting/generate_macos.sh
@@ -5,5 +5,6 @@ pip3 install virtualenv
 virtualenv venv
 
 venv/bin/pip3 install pandas
+venv/bin/pip3 install matplotlib
 
 venv/bin/python3 reporting/report_script.py
--- src/reporting/generate_windows.sh
+++ src/reporting/generate_windows.sh
@@ -5,5 +5,6 @@ pip install virtualenv
 virtualenv venv
 
 venv\bin\pip install pandas
+venv\bin\pip install matplotlib
 
 venv\bin\python reporting/report_script.py
