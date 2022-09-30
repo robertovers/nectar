a66b2a72dccb363d0fa6d1bf88b68a018d02c58a
Robert Overs <oversr14@gmail.com>
2022-09-30 21:46:30 +1000

    Add basic plotting

--- src/reporting/report_script.py
+++ src/reporting/report_script.py
@@ -3,7 +3,22 @@ import matplotlib.pyplot as plt
 
 def generate_report(datafile):
     sim_data = pd.read_csv(datafile);
-    sim_data.plot();
+
+    time = sim_data["time"];
+    nectar = sim_data["nectar"];
+    pollinated = sim_data["pollinated"];
+
+    figure, axis = plt.subplots(2, 1);
+
+    axis[0].plot(time, nectar);
+    axis[0].set_title("Nectar in Hive");
+
+    axis[1].plot(time, pollinated);
+    axis[1].set_title("No. of Pollinated Flowers");
+
+    plt.tight_layout();
+    plt.savefig("plot.pdf");
+    plt.show();
 
 if __name__ == '__main__':
     generate_report("reporting/sim_data.csv")
