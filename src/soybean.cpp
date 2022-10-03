0f4487266964ac7aca27ab7dd41984a86e35ead4
Robert Overs <oversr14@gmail.com>
2022-10-03 14:26:55 +1100

    Remove unused defined variable

--- src/soybean.cpp
+++ src/soybean.cpp
@@ -1,5 +1,4 @@
 #include "soybean.hpp"
-#define GENDER 3
 
 Soybean::Soybean(int x, int y, shared_ptr<sf::Color> colour, shared_ptr<sf::Color> nectarColour, shared_ptr<sf::Color> pollenColour, SoybeanOverlays soybeanOverlays) : Plant(x, y, colour, nectarColour, pollenColour) {
     // set up overlays
