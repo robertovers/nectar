e6ae438ef2474d447e9ccf5d259420cee661dedc
Robert Overs <oversr14@gmail.com>
2022-10-14 02:10:13 +1100

    Init nectar in default constructor

--- src/plant.cpp
+++ src/plant.cpp
@@ -1,7 +1,9 @@
 #include "plant.hpp"
 #include <iostream>
 
-Plant::~Plant() { }
+Plant::~Plant() { 
+    nectar = static_cast <float> (rand()) / static_cast <float> (RAND_MAX / MAX_NECTAR);
+}
 
 Plant::Plant(int x, int y, shared_ptr<sf::Color> colour, shared_ptr<sf::Color> nectarColour, shared_ptr<sf::Color> pollenColour) : Location(x, y, colour) {
     this->nectarColour = nectarColour;
