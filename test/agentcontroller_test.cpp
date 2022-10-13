ca53656f3fa3efa8e498f8af69b533e5f7329463
Lucille Joyce Grasso <lgra0008@student.monash.edu>
2022-10-13 23:14:57 +1100

    agentcontroller and planttest skeleton

---
+++ test/agentcontroller_test.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include "../src/agentController.hpp"
+
+// add agent
+TEST(AgentControllerTest, AddAgent) {
+
+}
+
+// remove agent
+TEST(AgentControllerTest, RemoveAgent) {
+
+}
+
+// update agents
+TEST(AgentControllerTest, UpdateAll) {
+
+}
+
+// draw agents
+TEST(AgentControllerTest, DrawAll) {
+
+}
---
+++ test/plant_test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include "../src/plant.hpp"
+
+// is a plant
+TEST(PlantTest, IsPlant) {
+
+}
+
+// pollination
+TEST(PlantTest, PlantPollination) {
+
+}
+
+// harvest nectar
+TEST(PlantTest, PlantNectarHarvest) {
+
+}
