41cc42e75b694fc6cf42b8103642142498929c0f
Lucille Joyce Grasso <lgra0008@student.monash.edu>
2022-10-14 12:21:15 +1100

    basic mocked update agent test

--- test/agentcontroller_test.cpp
+++ test/agentcontroller_test.cpp
@@ -1,7 +1,10 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include <algorithm>
 #include "../src/agentController.hpp"
 #include "../src/honeybee.hpp"
+#include "../src/agent.hpp"
+#include "../src/environment.hpp"
 
 TEST(AgentControllerTest, AddAgent) {
     AgentController ac = AgentController();
@@ -53,3 +56,46 @@ TEST(AgentControllerTest, RemoveAgent) {
 
     EXPECT_EQ(found, false); 
 }
+
+// mock agent class for update and draw tests
+class MockAgent : public HoneyBee {
+public:
+    MOCK_METHOD(void, draw, (sf::RenderTarget& target, sf::RenderStates states), (override, const));  // dont know if draw test will work bc of sf
+    MOCK_METHOD(void, update, (Environment& environment), (override));
+};
+
+TEST(AgentControllerTest, UpdateAgents) {
+    MockAgent agent1;
+    auto agentPointer = std::shared_ptr<Agent>(new MockAgent()); //std::make_shared<Agent>(agent1);
+    //MockAgent agent2;
+    //MockAgent agent3;
+    //MockAgent agents[] = { agent1, agent2, agent3 };
+
+    AgentController controller = AgentController();
+    controller.addAgent(agentPointer);
+    EXPECT_CALL(agent1, update(::testing::_)).Times(::testing::AtLeast(1));
+
+    /*for (int i = 0; i < 3; i++) {
+        controller.addAgent(shared_ptr<Agent>(agents[i]));
+        EXPECT_CALL(agents[i], update()).Times(::testing::AtLeast(1));
+    }*/
+    
+    auto env = Environment(3, 3, std::make_shared<sf::Color>());
+    controller.updateAgents(env);
+}
+
+//TEST(AgentControllerTest, DrawAgents) {
+//    MockAgent agent1;
+//    MockAgent agent2;
+//    MockAgent agent3;
+//    MockAgent agents[] = { agent1, agent2, agent3 };
+//
+//    AgentController controller = AgentController();
+//
+//    for (int i = 0; i < 3; i++) {
+//        controller.addAgent(agents[i]);
+//        EXPECT_CALL(agents[i], draw()).Times(::testing::AtLeast(1));
+//    }
+//
+//    controller.draw();
+//}
