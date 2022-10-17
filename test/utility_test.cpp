144d74c1c0ee1306467ab7dc07cd9f15e1cf32f2
JirehChen <jche0212@student.monash.edu>
2022-10-17 16:50:25 +1100

    Debugging check_limit test
    - Added utility_Test.cpp to executable list
    - replaced constant values with magic numbers -.-
    - added check_limits into tests.

--- test/utility_test.cpp
+++ test/utility_test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include "../src/utility.hpp"
 
+void eq_params(Parameters params, int rows, int columns, int scale, int bees, float prob);
+
 // Test that the check limits function adjusts parameter elements correctly
 TEST(UtilityTest, CheckLimits) {
 
@@ -13,30 +15,33 @@ TEST(UtilityTest, CheckLimits) {
     eq_params(params, 100, 100, 8, 50, 0.1);
 
     // Combinations of invalid values
-    params.rows = 0;
-    params.columns = DIMS_MAX+1;
-    params.scale = 0;
-    params.bees = BEES_MAX+1;
-    params.soybean_p = -1
-    eq_params(params, 1, DIMS_MAX, 1, BEES_MAX, 0.05);
+    params.rows = 0;            // Under min
+    params.columns = 401;      // Over max
+    params.scale = 0;           // Under min
+    params.bees = 10001;      // Over max
+    params.soybean_p = -1;       // Under min
+    params.check_limits();
+    eq_params(params, 1, 400, 1, 10000, 0.05);
 
-    params.rows = DIMS_MAX+1;
-    params.columns = 0;
-    params.scale = SCALE_MAX+1;
-    params.bees = -1;
-    params.soybean_p = 2;
-    eq_params(params, DIMS_MAX, 1, SCALE_MAX, 0, 1);
+    params.rows = 401;       // Over max
+    params.columns = 0;             // Under min
+    params.scale = 51;     // Over max
+    params.bees = -1;               // Under min
+    params.soybean_p = 2;           // Over max
+    params.check_limits();
+    eq_params(params, 400, 1, 50, 0, 1);
 
     // Combination of correct and incorrect values
-    params.soybean_p = 0.5
-    params.columns = DIMS_MAX+1;
-    eq_params(params, DIMS_MAX, DIMS_MAX, SCALE_MAX, 0, 0.5);
+    params.soybean_p = 0.5;
+    params.columns = 401;
+    params.check_limits();
+    eq_params(params, 400, 400, 50, 0, 0.5);
 }
 
 void eq_params(Parameters params, int rows, int columns, int scale, int bees, float prob){
     EXPECT_EQ(params.rows, rows);
     EXPECT_EQ(params.columns, columns);
     EXPECT_EQ(params.scale, scale);
-    EXPECT_EQ(params.bees, bee);
+    EXPECT_EQ(params.bees, bees);
     EXPECT_EQ(params.soybean_p, prob);
 }
